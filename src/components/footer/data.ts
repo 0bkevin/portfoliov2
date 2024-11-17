90b9a7342f4f1a35e605ac29e1f4a8d9d0c61c0a
Kevin Bravo <bravokevinto@gmail.com>
2024-11-17 10:50:53 -0400

    feat: add footer

---
+++ src/components/footer/data.ts
@@ -0,0 +1,19 @@
+export const footerLinks = [
+  { link: "#about", name: "About", index: 1 },
+  { link: "#portfolio", name: "Projects", index: 2 },
+  { link: "https://blog.kevinbravo.me/", name: "Blog", index: 3 },
+  {
+    link: "https://doc.clickup.com/25746702/d/h/rhq8e-525/61cc762b944d21c",
+    name: "AMA",
+    index: 4,
+  },
+];
+export const socialIcons = [
+  { icon: "bx bxl-twitter", link: "https://twitter.com/_BravoK", index: 1 },
+  { icon: "bx bxl-github", link: "https://github.com/mustafinho/", index: 2 },
+  {
+    icon: "bx bxl-linkedin",
+    link: "https://www.linkedin.com/in/bravokevin/",
+    index: 3,
+  },
+];
