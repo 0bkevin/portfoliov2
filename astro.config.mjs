696aa05655d10ea20508fc43f4bfc2cc1449e9e7
Kevin Bravo <bravokevinto@gmail.com>
2024-11-24 14:32:21 -0400

    feat: add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import svelte from '@astrojs/svelte';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()],
   output: 'server'
 });
