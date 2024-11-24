606f9b8ef4bba9241176aa7d560cbd03bdbbd157
Kevin Bravo <bravokevinto@gmail.com>
2024-11-24 15:13:11 -0400

    feat: add skills section

---
+++ src/components/skills/data.ts
@@ -0,0 +1,30 @@
+export const TOOLS = [
+  { name: "Fleex", level: "Advanced" },
+  { name: "Tenderly", level: "Advanced" },
+  { name: "Tally", level: "Advanced" },
+  { name: "Open Zeppelin", level: "Advanced" },
+  { name: "GitHub actions", level: "Advanced" },
+  { name: "Docker", level: "Intermediate" },
+  { name: "Power BI", level: "Advanced" },
+  { name: "Git/Github", level: "Advanced" },
+  { name: "IPFS", level: "Advanced" },
+  { name: "Alchemy/Infura", level: "Advanced" },
+];
+
+export const LANGUAGES = [
+  { name: "Solidity", level: "Advanced" },
+  { name: "Typescript", level: "Advanced" },
+  { name: "Rust", level: "Basic" },
+  { name: "SQL", level: "Intermediate" },
+  { name: "Python", level: "Intermediate" },
+  { name: "Cairo", level: "Intermediate" },
+];
+
+export const FRAMEWORKS = [
+  { name: "Hardhat", level: "Advanced" },
+  { name: "Brownie", level: "Advanced" },
+  { name: "Protostar", level: "Basic" },
+  { name: "NextJs", level: "Intermediate" },
+  { name: "Truffle", level: "Intermediate" },
+  { name: "Nile", level: "Intermediate" },
+];
