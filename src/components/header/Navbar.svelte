ac592f849e76001756e22f58b007d31c1bd80a8c
Kevin Bravo <bravokevinto@gmail.com>
2024-11-24 14:16:14 -0400

    feat: improve header

--- src/components/header/Navbar.svelte
+++ src/components/header/Navbar.svelte
@@ -1,17 +1,18 @@
 <script>
   import { onMount } from "svelte";
   import NAV_ITEMS from "./NavbarData.ts";
-  import "./navbar.css";
 
   let Toggle = false;
   let activeNav = "#home";
 
+  let stickyNav = false;
+
   const handleScroll = () => {
     const header = document.querySelector(".header");
-    if (window.scrollY >= 80) {
-      header.classList.add("scroll-header");
+    if (window.scrollY >= 50) {
+      stickyNav = true;
     } else {
-      header.classList.remove("scroll-header");
+      stickyNav = false;
     }
   };
 
@@ -31,10 +32,14 @@
   };
 </script>
 
-<nav class="h-[calc(var(--header-height)+1.5rem)] flex justify-between items-center gap-4 container">
-  <a href="index.html" class="nav__logo">Kevin Bravo</a>
-  <div class={Toggle ? "nav__menu show-menu" : "nav__menu"}>
-    <ul class="nav__list">
+<nav
+  class={`h-16 flex justify-between items-center gap-4 p-6 max-w-6xl mx-auto transition-transform ${
+    stickyNav ? "fixed-n" : ""
+  }`}
+>
+  <a href="./" class="font-semibold text-lg ">Kevin Bravo</a>
+  <div class={Toggle ? "nav__menu show-menu " : "nav__menu"}>
+    <ul class="hidden md:flex gap-6">
       {#each NAV_ITEMS as { name, link, id, icon }}
         <li class="nav__item" key={id}>
           <a
@@ -42,15 +47,34 @@
             on:click={() => setActive(link)}
             class={activeNav === link ? "nav__link active-link" : "nav__link"}
           >
-            <i class={`uil uil-${icon} nav__icon`}></i>
+            <!-- <i class={`uil uil-${icon} nav__icon`}></i> -->
             {name}
           </a>
         </li>
       {/each}
     </ul>
-    <i class="uil uil-times nav__close" on:click={toggleMenu}></i>
+
+    <!-- <button
+      class="uil uil-times"
+      on:click={toggleMenu}
+      aria-label="Close menu"
+      type="button"
+    >
+    </button>
   </div>
-  <div class="nav__toggle" on:click={toggleMenu}>
+  <button class="" on:click={toggleMenu} aria-label="Toggle menu">
     <i class="uil uil-apps"></i>
+  </button> -->
   </div>
 </nav>
+<style>
+  .fixed-n {
+    min-width: 100%;
+    position: fixed;
+    background-color: #fafafa;
+    box-shadow: 0 -1px 4px rgba(0,0,0,0.15);
+    top: 0;
+    width: 100%;
+    z-index: 1000;
+  }
+</style>
