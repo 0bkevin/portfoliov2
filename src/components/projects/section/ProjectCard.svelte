66cb47249727bceef78ac39184c68619b3f0e56c
Kevin Bravo <bravokevinto@gmail.com>
2024-11-24 13:55:28 -0400

    feat: add project section

---
+++ src/components/projects/section/ProjectCard.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { formatDateToDisplay } from "@lib/utils";
+  export let description: number,
+    demoUrl: string,
+    title: string,
+    slug: string,
+    pubYear: Date
+
+
+</script>
+
+<article
+  class="max-w-xs swiper-slide relative overflow-hidden rounded-lg shadow transition hover:shadow-lg aspect-square"
+>
+
+
+  <div
+    class="text-start relative bg-gradient-to-t from-gray-950 to-green-800 pt-36 h-full"
+  >
+    <div class="p-4 sm:p-6">
+      <time
+        datetime="2022-10-10"
+        class="text-slate-300 block text-xs font-medium text-white/90"
+        >{new Date(pubYear).getFullYear()}
+      </time>
+
+      <h3 class="mt-0.5 font-medium text-white  line-clamp-3 max-w-sm">
+        {title}
+      </h3>
+      <p class="mt-2 line-clamp-3 text-sm/relaxed text-slate-300">
+        {description}
+      </p>
+      <a
+        href={slug}
+        class="mt-2 bg-transparent text-white px-3 py-1 rounded-full text-xs border-slate-300 border hover:bg-white hover:text-black transition-colors hover:font-medium"
+      >
+        Read
+      </a>
+      {#if demoUrl}
+        <a
+          href={demoUrl}
+          class="mt-2 bg-transparent text-white px-3 py-1 rounded-full text-xs border-slate-300 border hover:bg-white hover:text-black transition-colors hover:font-medium"
+        >
+          Demo
+        </a>
+      {/if}
+    </div>
+  </div>
+</article>
---
+++ src/components/projects/section/Swiper.svelte
@@ -0,0 +1,96 @@
+<script
+  lang="ts"
+  src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"
+>
+  import "swiper/swiper-bundle.css";
+  import { register } from "swiper/element/bundle";
+  import { onMount } from "svelte";
+  import ProjectCard from "./ProjectCard.svelte";
+
+  import { formatDateToDisplay } from "@lib/utils.ts";
+  register();
+
+  export let projects: {
+    data: {
+      description: number;
+      title: string;
+      slug: string;
+      pubYear: Date;
+      image: string;
+      demoUrl: string;
+      repoUrl: string;
+    };
+    slug: string;
+  }[];
+
+  onMount(() => {
+    const swiperEl = document.querySelector("swiper-container");
+    if (swiperEl) {
+      Object.assign(swiperEl, {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        speed: 500,
+        loop: true,
+        autoplayDelay: 2000,
+        autoplayDisableOnInteraction: false,
+        pagination: {
+          clickable: true,
+        },
+        breakpoints: {
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+        },
+      });
+      swiperEl.initialize();
+    }
+  });
+</script>
+
+<div class="max-w-7xl xl:mx-auto">
+  <swiper-container class="mySwiper" init="false" navigation="true">
+    {#each projects as project}
+      <swiper-slide>
+        <ProjectCard
+          description={project.data.description}
+          demoUrl={project.data.demoUrl}
+          image={project.data.image}
+          pubYear={project.data.pubYear}
+          slug={`${project.collection}/${project.slug}`}
+          title={project.data.title}
+        />
+      </swiper-slide>
+    {/each}
+  </swiper-container>
+</div>
+
+<style>
+  swiper-container {
+    width: 100%;
+    height: 100%;
+  }
+
+  swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+</style>
