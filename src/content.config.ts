eec99c24c17736c9ab803028f39d47bac4da4ca6
Kevin Bravo <bravokevinto@gmail.com>
2024-12-03 18:16:38 -0400

    feat: update collections to new api

---
+++ src/content.config.ts
@@ -0,0 +1,48 @@
+import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blogCollection = defineCollection({
+  loader: glob({ pattern: "**\/*.md", base: "./src/content/blogs" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.coerce.date(),
+    description: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }),
+    tags: z.array(z.string()),
+    language: z.enum(['en', 'es']),
+    footnote: z.string().optional(),
+    // Transform a date string (e.g. "2022-07-08") to a Date object
+    updatedDate: z.string().transform((str) => new Date(str)),
+    canonicalURL: z.string().url(),
+
+  }),
+});
+
+
+const projectCollection = defineCollection({
+  loader: glob({ pattern: "**\/*.md", base: "./src/content/proyects" }),
+  schema: z.object({
+    title: z.string(),
+    pubYear: z.coerce.date(),
+    description: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }),
+    tags: z.array(z.string()),
+    demoUrl: z.string().optional(),
+    repoUrl: z.string().optional(),
+    studyCaseUrl: z.string().optional(),
+    language: z.enum(['en', 'es']),
+    footnote: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  'blog': blogCollection,
+  'projects': projectCollection,
+};
+
