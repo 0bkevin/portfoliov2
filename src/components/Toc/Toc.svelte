6d34f55e7bc360094237f956d0b1b8493ceae01c
Kevin Bravo <bravokevinto@gmail.com>
2024-12-04 00:36:59 -0400

    feat: toc component

---
+++ src/components/Toc/Toc.svelte
@@ -0,0 +1,60 @@
+<script>
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
+  import TocItem from "./TocItem.svelte";
+
+  let headings = writable([]);
+  let isOpen = writable(false);
+
+  onMount(() => {
+    const elements = document.querySelectorAll("h1, h2, h3");
+    const headingData = Array.from(elements).map((heading) => ({
+      id:
+        heading.id ||
+        heading.textContent?.toLowerCase().replace(/\s+/g, "-") ||
+        "",
+      text: heading.textContent || "",
+      level: parseInt(heading.tagName[1]),
+    }));
+    headings.set(headingData);
+  });
+
+  const toggleOpen = () => {
+    isOpen.update((n) => !n);
+  };
+</script>
+
+<div class="md:max-w-xs overflow-x-hidden">
+  <!-- Toggle Button -->
+  <button on:click={toggleOpen} class="toggle-button rounded-full px-3 py-2 top-4 md:top-64 opacity-50 md:opacity-100 hover:scale-105">
+    {#if $isOpen}
+      <i class="bx bx-x text-lg font-medium"></i>
+    {:else}
+      <i class="bx bx-menu-alt-right text-lg font-medium"></i>
+    {/if}
+  </button>
+
+  <!-- Table of Contents -->
+  <nav
+    class="md:max-w-xs p-4 rounded-md transition-all duration-300 ease-in-out overflow-y-auto bg-white md:bg-transparent {$isOpen
+      ? 'translate-y-0 opacity-100'
+      : '-translate-y-full opacity-0'}"
+  >
+    <p class="font-semibold mb-3 text-lg">Table of Contents</p>
+    <ul class="space-y-1.5 max-w-[15rem]">
+      {#each $headings as heading}
+        <TocItem item={heading} />
+      {/each}
+    </ul>
+  </nav>
+</div>
+
+<style>
+  .toggle-button {
+    position: fixed;
+    right: 1rem;
+    background-color: ghostwhite;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+    z-index: 50;
+  }
+</style>
---
+++ src/components/Toc/TocItem.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let item = {
+    text: '',
+    level: 0,
+    id: ''
+  };
+
+  console.log(item)
+</script>
+
+{#if item.level === 2}
+<li class="font-medium text-sm text-gray-500 hover:text-black truncate ">
+  <a href={`#${item.id}`} on:click={() => isOpen.set(false)}>
+    {item.text}
+  </a>
+</li>
+{:else if item.level === 3}
+<li class="ml-4 text-sm text-gray-500 hover:text-black truncate ">
+  <a href={`#${item.id}`} on:click={() => isOpen.set(false)}>
+    * {item.text}
+  </a>
+</li>
+{:else if item.level === 4}
+<li class="ml-8 text-xs text-gray-500 hover:text-black truncate ">
+  <a href={`#${item.id}`} on:click={() => isOpen.set(false)}>
+    * {item.text}
+  </a>
+</li>
+{/if}
