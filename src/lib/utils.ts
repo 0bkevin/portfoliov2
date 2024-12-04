c0d062c521f64feb06dd8116ee9cc741dc51849f
Kevin Bravo <bravokevinto@gmail.com>
2024-12-04 00:42:18 -0400

    feat: add util to get reading time

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,15 @@
-
-export const formatDateToDisplay = (dateString: Date | string) =>{
-  return new Date(dateString).toLocaleDateString("es-VE", {
+export const formatDateToDisplay = (dateString: Date | string) => {
+  return new Date(dateString).toLocaleDateString("en-US", {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
   });
-}
+};
+
+export const getReadingTime = (body: string |  undefined) => {
+  const wordsPerMinute = 200;
+  if (!body) return 0;
+  const wordCount = body.split(/\s+/).length;
+  const readingTime = Math.ceil(wordCount / wordsPerMinute);
+  return readingTime;
+};
