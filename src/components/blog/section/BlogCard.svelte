3f8c3b16be0c163204fbcf3027230287379f0342
Kevin Bravo <bravokevinto@gmail.com>
2024-12-04 00:44:10 -0400

    feat: handle new api and make improvements

--- src/components/blog/section/BlogCard.svelte
+++ src/components/blog/section/BlogCard.svelte
@@ -3,22 +3,27 @@
   export let description: number,
     title: string,
     slug: string,
+    tags: string[],
     pubDate: Date,
     image: {
       url: string;
       alt: string;
     };
+
+    console.log(title)
 </script>
 
 <article
   class="max-w-xs swiper-slide relative overflow-hidden rounded-lg shadow transition hover:shadow-lg aspect-square"
 >
+
+{#if image}
   <img
     alt={image.alt}
     src={image.url}
     class="absolute inset-0 h-full w-full object-cover"
   />
-
+{/if}
   <div
     class="text-start relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-28 h-full"
   >
--- src/components/blog/section/Swipper.svelte
+++ src/components/blog/section/Swipper.svelte
@@ -1,25 +1,14 @@
 <script lang="ts">
+  
   import "swiper/swiper-bundle.css";
   import { register } from "swiper/element/bundle";
   import { onMount } from "svelte";
   import BlogCard from "./BlogCard.svelte";
+  import type { CollectionEntry } from 'astro:content';
 
-  import { formatDateToDisplay } from "@lib/utils.ts";
   register();
 
-  export let blogs: {
-    data: {
-      description: number;
-      title: string;
-      slug: string;
-      pubDate: Date;
-      image: {
-        url: string;
-        alt: string;
-      };
-    };
-    slug: string;
-  }[];
+  export let blogs: CollectionEntry<'blogs'>[];
 
   onMount(() => {
     const swiperEl = document.querySelector("swiper-container");
@@ -63,7 +52,7 @@
           image={blog.data.image}
           pubDate={blog.data.pubDate}
           tags={blog.data.tags}
-          slug={`${blog.collection}/${blog.slug}`}
+          slug={`${blog.collection}/${blog.id}`}
           title={blog.data.title}
         />
       </swiper-slide>
