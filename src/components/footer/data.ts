22278d6006455ac7ec8bb5a84622571d2f05bf06
Kevin Bravo <bravokevinto@gmail.com>
2025-01-05 12:27:49 -0400

    refactor: footer

--- src/components/footer/data.ts
+++ src/components/footer/data.ts
@@ -1,12 +1,17 @@
 export const footerLinks = [
   { link: "#about", name: "About", index: 1 },
   { link: "#projects", name: "Projects", index: 2 },
-  { link: "https://blog.kevinbravo.me/", name: "Blog", index: 3 },
+  { link: "/0b", name: "Blog", index: 3 },
   {
-    link: "https://doc.clickup.com/25746702/d/h/rhq8e-525/61cc762b944d21c",
+    link: "/me/ama",
     name: "AMA",
     index: 4,
   },
+  {
+    link: "/me/who-is-kevin-bravo",
+    name: "Who am I?",
+    index: 4,
+  }, 
 ];
 export const socialIcons = [
   { icon: "bx bxl-twitter", link: "https://twitter.com/0bkevin", index: 1 },
