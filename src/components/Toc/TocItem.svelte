40843a59fbf35e8f25b642ccf01d483038eaf677
Kevin Bravo <bravokevinto@gmail.com>
2025-01-21 17:18:01 -0400

    feat: improve toc points

--- src/components/Toc/TocItem.svelte
+++ src/components/Toc/TocItem.svelte
@@ -14,15 +14,19 @@
     </a>
   </li>
 {:else if item.level === 3}
-  <li class="ml-4 text-sm text-gray-500 hover:text-black truncate">
+  <li
+    class="ml-4 text-sm text-gray-500 hover:text-black truncate flex items-center gap-1"
+  >
+    <i class="bx bxs-circle !text-[0.30rem]"></i>
     <a href={`#${item.id}`} on:click={() => isOpen.set(false)}>
-      <i class='bx bxs-circle !text-[0.50rem]'></i> {item.text}
+      {item.text}
     </a>
   </li>
 {:else if item.level === 4}
-  <li class="ml-8 text-xs text-gray-500 hover:text-black truncate">
+  <li class="ml-8 text-xs text-gray-500 hover:text-black truncate gap-1">
+    <i class="bx bxs-circle !text-[0.30rem]"></i>
     <a href={`#${item.id}`} on:click={() => isOpen.set(false)}>
-      <i class='bx bxs-circle !text-[0.50rem]'></i> {item.text}
+      {item.text}
     </a>
   </li>
 {/if}
