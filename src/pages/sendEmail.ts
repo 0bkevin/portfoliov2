cca29ffea0d625ed30b08ca06eca60b5e2b483d4
Kevin Bravo <bravokevinto@gmail.com>
2025-02-03 17:47:10 -0400

    refactor: quit unused variable

--- src/pages/sendEmail.ts
+++ src/pages/sendEmail.ts
@@ -2,19 +2,22 @@ import type { APIRoute } from "astro";
 import { sendEmailMessage } from "../lib/sendEmailMessage";
 
 export const POST: APIRoute = async ({ request }) => {
-    if (request.headers.get("Content-Type") === "application/json") {
+  if (request.headers.get("Content-Type") === "application/json") {
     const body = await request.json();
-    const response = await sendEmailMessage({
+    await sendEmailMessage({
       name: body.name,
       email: body.email,
       subject: body.subject,
       message: body.message,
     });
-    return new Response(JSON.stringify({
-      message: "Email sent successfully",
-    }), {
-      status: 200
-    })
+    return new Response(
+      JSON.stringify({
+        message: "Email sent successfully",
+      }),
+      {
+        status: 200,
+      }
+    );
   }
   return new Response(null, { status: 400 });
 };
