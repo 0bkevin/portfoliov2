9a6140f92a2f6fd5d28ffd39c69635787e9145ce
Kevin Bravo <bravokevinto@gmail.com>
2025-02-03 18:29:25 -0400

    feat: add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), sitemap()],
-  output: 'server'
+  output: 'server',
+  adapter: cloudflare()
 });
