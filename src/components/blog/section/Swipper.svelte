aa05f328c6281821017cb9608a7fa21bf2fc3ffb
Kevin Bravo <bravokevinto@gmail.com>
2026-04-22 16:10:05 -0400

    fix: avoid all selection

--- src/components/blog/section/Swipper.svelte
+++ src/components/blog/section/Swipper.svelte
@@ -37,7 +37,7 @@
   });
 </script>
 
-<div class="max-w-[70rem] mx-auto px-12 md:px-16 relative group">
+<div class="max-w-[70rem] mx-auto px-12 md:px-16 relative">
   <!-- Custom Prev Arrow -->
   <button class="swiper-button-prev-custom absolute left-0 md:left-2 top-[40%] -translate-y-1/2 z-10 p-2 text-surface-500 hover:text-accent-500 transition-colors disabled:opacity-50 disabled:cursor-not-allowed hidden sm:block">
     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
